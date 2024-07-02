60acc73cc94d51fc3112efa072c5a95584063bcc
Vishal bala <vishal.bala.100@gmail.com>
2024-07-02 15:05:25 +0530

    update the p.json

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: "/Wether-App/", // Correct base path for GitHub Pages
   plugins: [react()],
   build: {
-    outDir: "build", // Make sure this matches your deploy script
+    outDir: "build", // Ensure this matches your deploy script
   },
 });
